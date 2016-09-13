993b026c14ae85ac355891d807844f6f92bbf64d
Nitesh Patel <nitesh.patel@hotmail.com>
2016-09-13 14:17:46 +0100

    Prefix messages with user name

--- slack_to_habitica/views.py
+++ slack_to_habitica/views.py
@@ -11,26 +11,22 @@ def process_slack_message(request):
     send_message(
         request.POST.get('user_name'), 
         request.POST.get('text'))
+        
     
-    return HttpResponse(request.body)
-    
-    
-def send_message(user, text):
+def send_message(user, text):    
+    api_user = os.environ['HABITICA_APIUSER']
+    api_key = os.environ['HABITICA_APIKEY']
     group_id = os.environ['HABITICA_GROUPID']
     
     habitica_url = 'https://habitica.com/api/v3/groups/%s/chat' % group_id
-    
-    api_user = os.environ['HABITICA_APIUSER']
-    api_key = os.environ['HABITICA_APIKEY']
-    
+       
     headers = {
         'x-api-user': api_user, 
         'x-api-key': api_key
-    }
-    
+    }    
     data = {
         'groupId': group_id,
-        'message': text
+        'message': '<%s says> %s' % (user, text)
     }
     
     response = requests.post(habitica_url, headers=headers, data=data)
