318a66d403f7f194e5319c98ffb8a11e8a3aad6c
Nitesh Patel <nitesh.patel@hotmail.com>
2016-09-14 18:09:22 +0100

    Move sync command into a django style command

---
+++ habitica_slack/management/commands/sync_messages_to_slack.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from habitica_slack import actions
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        self.stdout.write('running sync_messages_to_slack...')
+        actions.sync_messages_to_slack()
+
+        self.stdout.write('done.')
